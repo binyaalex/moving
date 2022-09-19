3bb160c523f808e0e5404ace88d975a08c018cec
ziv-hagai <zivhag@gmail.com>
2022-09-19 12:29:32 +0300

    add form to online s.

--- src/components/storage/OnlineStorage.js
+++ src/components/storage/OnlineStorage.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Storage.css";
-
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 //components
 
 import MainBtn from '../storing/generalComponents/MainBtn'
@@ -19,19 +21,6 @@ import businessStorage from "../../assets/images/PS/businessStorage.jpg";
 import online from "../../assets/images/PS/online.jpg";
 
 const OnlineStorage = () => {
-
-  const servicesArr = [
-    {
-      header: 'אונליין',
-      img: privateStorage,
-      link: ''
-    },
-    {
-      header: 'בעזרת נציג',
-      img: businessStorage,
-      link: ''
-    },
-  ]
 
   return (
     <>
@@ -53,28 +42,71 @@ const OnlineStorage = () => {
 
       <Pros />
 
-      <div className="servicesDiv">
-        <h1 className="homeTitle">דרכי הזמנה</h1>
-        <div className="services">
-          {servicesArr.map((el, i) => {
-            return (
-              <div className="service" key={i}>
-                <Link to={el.link}>
-                  <img src={el.img} />
-                </Link>
-                <h1>{el.header}</h1>
-              </div>
-            )
-          })}
+      <div className="onlineStorageForm">
+        <h1 className="homeTitle">פרטי הזמנה</h1>
+        <div className="contactDiv">
+
+          <Form className="formBox" variant="warning">
+            <Row className="mb-3 row">
+
+              <Form.Group as={Col} className="mb-3" variant="warning" controlId="formBasicName">
+                <Form.Label>אזור</Form.Label>
+                <Form.Select aria-label="Default select example">
+                  <option>. . .</option>
+                  <option value="center">מרכז</option>
+                  <option value="sharon">שרון</option>
+                  <option value="north sharon">צפון השרון</option>
+                  <option value="north">צפון</option>
+                  <option value="shfela">שפלה</option>
+                  <option value="south">דרום</option>
+                  <option value="jerusalem">ירושלים והסביבה</option>
+                </Form.Select>
+
+              </Form.Group>
+
+              <Form.Group variant="warning" as={Col} className="mb-3 warning" controlId="formBasicEmail">
+                <Form.Label>תאריך</Form.Label>
+                <Form.Control type="date" placeholder='01/01/2023' />
+                <Form.Check
+                  reverse
+                  type='checkbox'
+                  label='גמיש'
+                  variant="warning"
+                />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3 row">
+
+              <Form.Group as={Col} className="mb-3" controlId="formBasicPhone">
+                <Form.Label>טלפון</Form.Label>
+                <Form.Control type="phone" placeholder="הטלפון שלי" />
+
+              </Form.Group>
+
+              <Form.Group as={Col} className="mb-3" controlId="formBasicity">
+                <Form.Label>עיר</Form.Label>
+                <Form.Control type="text" placeholder="העיר שלי" />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3 row">
+
+              <Form.Group className="mb-3 text" controlId="formBasicEmail">
+                <Form.Label>פנייה</Form.Label>
+                <Form.Control
+                  // as="textarea"
+                  placeholder="הפנייה שלי"
+                  style={{ height: '100px', width: '100%' }}
+                />
+              </Form.Group>
+            </Row>
+            <div className="contact-btnDiv row">
+              <MainBtn text='יצירת קשר' link='#' />
+            </div>
+          </Form>
         </div>
       </div>
 
-      <div className="about blog">
-        <h1 className="homeTitle">איך לארוז דירה</h1>
-        <div className="clipWrapper">
-          <video className="clip" src={clip} autoPlay={true} muted={true} controls />
-        </div>
-      </div>
+
 
       <Reviews />
       <Footer />
