277e0e02a8376c2bff6c4a9afc4fe5242b019d7f
unknown <binyaalex@gmail.com>
2022-09-19 12:23:10 +0300

    making transport few items page

--- src/components/transportFewItems/TransportFewItems.js
+++ src/components/transportFewItems/TransportFewItems.js
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./TransportFewItems.css";
 
+// bootstrap
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 //components
 
 import moving from "../../assets/images/moving.jpg";
@@ -93,6 +98,66 @@ const TransportFewItems = () => {
                         )
                     })}
                 </div>
+                
+                <Form className="formBox">
+                    <Row className="mb-3 row">
+                        <Form.Group as={Col} className="mb-3" controlId="formBasicFrom">
+                            <Form.Label>מהיכן אתה עובר?</Form.Label>
+                            <Form.Control type="text" placeholder="באר שבע" />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3 row">
+                        <Form.Group as={Col} className="mb-3" controlId="formBasicDestination">
+                            <Form.Label>להיכן אתה עובר?</Form.Label>
+                            <Form.Control type="text" placeholder="נתניה" />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3 row">
+                        <Form.Group as={Col} className="mb-3" controlId="formBasicDate">
+                            <Form.Label>מתי אתה עובר?</Form.Label>
+                            <Form.Control type="date" placeholder="" />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3 row">
+                        <Form.Group className="mb-3 text" controlId="formBasicList">
+                            <Form.Label>רשימת פריטים</Form.Label>
+                            <Form.Control
+                            // as="textarea"
+                            placeholder="4 מיטות 3 ארונות..."
+                            style={{ height: '100px', width: '100%' }}
+                            />
+                        </Form.Group>
+                    </Row>
+
+                    <Row className="mb-3 row">
+                        <Form.Group as={Col} className="mb-3" controlId="formBasicFile">
+                            <Form.Label>העלאת תמונות או וידאו (לא חובה)</Form.Label>
+                            <Form.Control type="file" placeholder="בחר קובץ" />
+                        </Form.Group>
+                    </Row>
+
+                    <Form.Group as={Col} className="mb-3" controlId="formBasicName">
+                        <Form.Label>פרטים ליצירת קשר</Form.Label>
+                        <Form.Control type="text" placeholder="שם מלא" />
+                        <Form.Control
+                            type="email" 
+                            placeholder="אימייל" 
+                            style={{ margin: '10px 0' }}
+                        />
+                        <Form.Control type="phone" placeholder="טלפון" />
+                    </Form.Group>
+
+                    <div className="contact-btnDiv row">
+                        <MainBtn 
+                            text='שלח' 
+                            link='#' 
+                            style={{ padding: '5px 25px' }}
+                        />
+                    </div>
+                </Form>
 
             </div>
 
