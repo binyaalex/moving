1faca7b3a71296ef4d7bd3016db76bd5518004cf
ziv-hagai <zivhag@gmail.com>
2022-09-21 12:04:16 +0300

    move categoryArr to itemList

--- src/components/repeats/itemList/ItemList.js
+++ src/components/repeats/itemList/ItemList.js
@@ -2,11 +2,50 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import './ItemList.css'
 
+const categoryArr = [
+  {
+    img: "/images/moving/closet.jpg",
+    title: "ארונות",
+  },
+  {
+    img: "/images/moving/sofa.jpg",
+    title: "ספות",
+  },
+  {
+    img: "/images/moving/bed.jpg",
+    title: "מיטות",
+  },
+  {
+    img: "/images/moving/wm.jpg",
+    title: "מכונות כביסה",
+  },
+  {
+    img: "/images/moving/table.jpg",
+    title: "שולחנות",
+  },
+  {
+    img: "/images/moving/chair.jpg",
+    title: "כסאות",
+  },
+  {
+    img: "/images/moving/tv.jpg",
+    title: "טלויזיות",
+  },
+  {
+    img: "/images/moving/dresser.jpg",
+    title: "שידות",
+  },
+  {
+    img: "/images/moving/stuff.jpg",
+    title: "שונות",
+  },
+];
+
 const ItemList = (props) => {
 
   return (
     <div className="ItemsList">
-      {props.itemsArr.map((el, i) => {
+      {(props.itemsArr || categoryArr).map((el, i) => {
         return (
           <div className="item" key={i}>
             <img src={el.img} />
