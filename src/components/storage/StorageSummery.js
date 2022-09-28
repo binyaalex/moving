a8c5eceac7e1a4168a4d58f0fcb8b3efc05c5040
unknown <binyaalex@gmail.com>
2022-09-28 11:51:06 +0300

    finished storge summery

--- src/components/storage/StorageSummery.js
+++ src/components/storage/StorageSummery.js
@@ -17,13 +17,13 @@ import PaymentSuccess from "../repeats/paymentSuccess/PaymentSuccess";
 
 //images
 import bannerImg from "../../assets/images/TII/0d29cde6ea92435fc56819d5d1f3d55a.jpg";
-import mainImg from "../../assets/images/MT/4.jpg";
-import galleryImg1 from "../../assets/images/MT/6.jpg";
-import galleryImg2 from "../../assets/images/MT/GettyImages-135385164_pczbmr.jpg";
-import galleryImg3 from "../../assets/images/MT/moving-checklist-2000-6ec63f4c4aa94bf9b9cb32f80bfd97a7.jpg";
-import galleryImg4 from "../../assets/images/MT/6-Common-Mistakes-to-Avoid-When-Hiring-a-Moving-Company.jpeg";
-import galleryImg5 from "../../assets/images/MT/AdobeStock_300477109-min_a23ba322c2da1cd76ade9dd3d3983ed1_2000.jpeg";
-import galleryImg6 from "../../assets/images/MT/movers-collegeville.jpg";
+import mainImg from "../../assets/images/storing.jpg";
+import galleryImg1 from "../../assets/images/PS/912_0126.jpg";
+import galleryImg2 from "../../assets/images/PS/Boost_18_1101_149.jpg";
+import galleryImg3 from "../../assets/images/PS/Stokado_Self_Storage1lr-768x512-jpg.jpg";
+import galleryImg4 from "../../assets/images/PS/businessStorage.jpg";
+import galleryImg5 from "../../assets/images/PS/maxresdefault.jpg";
+import galleryImg6 from "../../assets/images/PS/privateStorage.jpg";
 
 const StorageSummery = () => {
   const [totalPrice, setTotalPrice] = useState(2000);
@@ -84,12 +84,12 @@ const StorageSummery = () => {
         <img src={bannerImg} />
         <div className="banner_content">
           <div className="banner_title">
-            <h1>הזמנת הובלה אונליין</h1>
+            <h1>הזמנת אחסנה אונליין</h1>
             <p>רק למלא פרטים ולשלוח</p>
           </div>
           <div className="btnDiv">
             <MainBtn text='חזרה' link='/onlinestorage' />
-            <MainBtn text='הזמנה בעזרת נציג' link='/transportgetback' isLight={true} />
+            <MainBtn text='הזמנה בעזרת נציג' link='/getbackStorage' isLight={true} />
           </div>
         </div>
       </div>
@@ -102,7 +102,7 @@ const StorageSummery = () => {
           <div className="imagesSide">
             <SideGallery
               mainImg={mainImg}
-              header='שירותי הובלות'
+              header='שירותי אחסנה'
               galleryArr={galleryArr}
             />
           </div>
